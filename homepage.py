376216a5f2285c621b073539ab637913f4aff0a5
Danielbrey <72413011+Danielbrey@users.noreply.github.com>
2021-11-10 16:54:31 -0500

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return render_template('homepage.html')
+   return render_template('mainpage.html')
 
 if __name__ == '__main__':
    app.run()
